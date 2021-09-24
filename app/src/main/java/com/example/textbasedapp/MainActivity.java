b44666ff672b0494af7d8a83c37e963b74012d9c
Sumit <sumitchauhan202020@gmail.com>
2021-09-24 17:09:44 +0530

    textbasedapp

---
+++ app/src/main/java/com/example/textbasedapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.textbasedapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
